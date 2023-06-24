0868f8e42108f99c9078008faeac977fa73aaec9
Ana Laura Siczkowski <sicz.ana@hotmail.com>
2023-06-24 13:25:49 -0300

    todo

---
+++ ms-gateway/src/main/java/com/dh/msgateway/configuration/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.dh.msgateway.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@EnableWebFluxSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http.authorizeExchange().anyExchange().authenticated();
+                //.and().oauth2Login();
+        return http.build();
+    }
+}
